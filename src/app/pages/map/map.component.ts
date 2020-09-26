39357a74dd70dfb5ba89695b2ce3f31c572e9c9c
daniel-dantas <daniel.dantas.developer@gmail.com>
2020-09-26 14:23:17 -0300

    Implementando funcionalidades da tela que irá exibir o mapa

--- src/app/pages/map/map.component.ts
+++ src/app/pages/map/map.component.ts
@@ -1,4 +1,8 @@
+import { DataInfoService } from './../../services/data-info.service';
+import { County } from './../../models/County';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { map, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-map',
@@ -7,9 +11,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapComponent implements OnInit {
 
-  constructor() { }
+  public county: County;
+  public svgData: string;
+  public countyAuxName: string;
+
+  constructor(
+    private dataInfoService: DataInfoService,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.loadCounty();
+  }
+
+  loadCounty(): void{
+    this.route.params.pipe(
+      map(params => { this.countyAuxName = params.county; return params.county; }),
+      switchMap(county => this.dataInfoService.getSVG(county))
+    ).subscribe(svg => {
+      this.svgData = svg;
+    });
+
+    this.route.params.pipe(
+      switchMap(params => this.dataInfoService.getCountys(params.state)),
+      switchMap(countys => countys.filter(county => county.nome === this.countyAuxName))
+    ).subscribe(county => {
+      this.county = county;
+    });
+
   }
 
 }
